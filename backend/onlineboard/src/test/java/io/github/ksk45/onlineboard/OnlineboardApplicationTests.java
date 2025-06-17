212d2d676ef7f69ae59c10ee7609a835bdd1a4a3
ksk45 <oooouu1k@gmail.com>
2025-06-17 23:50:30 +0900

    テスト用application.properties作成。 テスト通過設定（TODOあり）

--- backend/onlineboard/src/test/java/io/github/ksk45/onlineboard/OnlineboardApplicationTests.java
+++ backend/onlineboard/src/test/java/io/github/ksk45/onlineboard/OnlineboardApplicationTests.java
@@ -2,8 +2,12 @@ package io.github.ksk45.onlineboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+/** TODO: テスト用のプロファイルを設定をアノテーションではなく全体で設定するようにする
+ * （現在は自動でapplication.propertiesが使用されてしまうため、アノテーションで直接指定） */
+@ActiveProfiles("test")
 class OnlineboardApplicationTests {
 
 	@Test
