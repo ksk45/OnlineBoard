1a0103a3d3ebe42d74c18b483352360b5d0c9c07
ksk45 <oooouu1k@gmail.com>
2025-06-27 19:05:24 +0900

    ログイン画面　インプットコンポーネント作成

---
+++ frontend/src/components/Auth/AuthInput.tsx
@@ -0,0 +1,112 @@
+import { Eye, LockKeyhole, Mail, User } from "lucide-react";
+import { useState, type JSX } from "react";
+import { tv, type VariantProps } from "tailwind-variants";
+
+// cssの条件分岐を記載
+const inputField = tv({
+  // slots：デフォルトのcssを記述
+  slots: {
+    label: "mb-2 block text-sm font-medium text-gray-900 dark:text-white",
+    input:
+      "block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm",
+    error: "mt-2 text-sm text-red-600 dark:text-red-500",
+  },
+  // variants: 条件に応じたcssを記述
+  variants: {
+    error: {
+      true: {
+        label: "text-red-700 dark:text-red-500",
+        input:
+          "border-red-500 bg-red-50 text-red-900 placeholder-red-700 focus:border",
+      },
+    },
+  },
+});
+
+// props定義
+type AuthInputProps = VariantProps<typeof inputField> & {
+  errorMessage?: string;
+  type: string;
+};
+
+// メイン処理
+const AuthInput = (props: AuthInputProps) => {
+  const { label, input, error } = inputField({
+    error: !!props.errorMessage,
+  });
+
+  // inputのtype定義
+  const types: Record<string, string> = {
+    email: "email",
+    pass: "password",
+  };
+  const type = types[props.type] || "text";
+
+  // input名定義
+  const inputLabels: Record<string, string> = {
+    email: "Email Address",
+    pass: "Password",
+    pass_new: "Password",
+    pass_conf: "Confirm Password",
+    name: "Name",
+  };
+  const inputLabel = inputLabels[props.type] || "Input value";
+
+  // placeholderのアイコン定義
+  const placeholderIconsClass = "absolute ml-2 text-gray-400";
+
+  const placeholderIcons: Record<string, JSX.Element> = {
+    email: <Mail className={placeholderIconsClass} />,
+    pass: <LockKeyhole className={placeholderIconsClass} />,
+    pass_new: <LockKeyhole className={placeholderIconsClass} />,
+    pass_conf: <LockKeyhole className={placeholderIconsClass} />,
+    name: <User className={placeholderIconsClass} />,
+  };
+  const placeholderIcon = placeholderIcons[props.type] || null;
+
+  // placeholder定義
+  const placeholders: Record<string, string> = {
+    email: "Enter your email",
+    pass: "Enter your password",
+    pass_new: "Create a password",
+    pass_conf: "Confirm your password",
+    name: "Enter your Name",
+  };
+  const placeholder = placeholders[props.type] || "Enter value";
+
+  // pass確認ボタン定義
+  const [isVisible, setIsVisible] = useState(false);
+  const togglePassVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <div>
+      <label className={label()}>{inputLabel}</label>
+      <div className="relative flex items-center">
+        {placeholderIcon}
+        <input
+          type={isVisible ? "text" : type}
+          placeholder={placeholder}
+          className={`${input()} pl-10`}
+          required
+        />
+        {(props.type === "pass" || props.type === "pass_new") && (
+          <button
+            type="button"
+            onClick={togglePassVisibility}
+            className="absolute right-2 text-gray-400"
+            tabIndex={-1}
+          >
+            <Eye />
+          </button>
+        )}
+      </div>
+      {props.errorMessage && (
+        <div className={error()}>{props.errorMessage}</div>
+      )}
+    </div>
+  );
+};
+
+export default AuthInput;
--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,23 +1,50 @@
-import { useState } from 'react'
-import AuthHeader from '../components/Auth/AuthHeader'
-import type { AuthMode } from '../types/auth';
-import AuthMainButton from '../components/Auth/AuthMainButton';
+import { useState, type JSX } from "react";
+import AuthHeader from "../components/Auth/AuthHeader";
+import AuthInput from "../components/Auth/AuthInput";
+import AuthMainButton from "../components/Auth/AuthMainButton";
+import type { AuthMode } from "../types/auth";
 
 const AuthPage = () => {
-
   const [authMode, setAuthMode] = useState<AuthMode>("signIn");
+
+  // それぞれのフォームを定義
+  // サインイン
+  const signInForm = () => (
+    <>
+      <AuthInput type="email" />
+      <AuthInput type="pass" errorMessage="不正な値です" />
+      <AuthMainButton authMode={authMode} />
+    </>
+  );
+  // サインアップ
+  const signUpForm = () => (
+    // TODO: サインアップ画面作成時に定義
+    <></>
+  );
+
+  // ゲストログイン
+  const guestForm = () => (
+    //TODO: ゲストログイン画面作成時に定義
+    <></>
+  );
+
+  // authModeに応じてフォームを切り替え
+  const inputForm: Record<AuthMode, JSX.Element> = {
+    signIn: signInForm(),
+    signUp: signUpForm(),
+    guest: guestForm(),
+  };
 
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 p-4">
       <div className="max-w-md w-full space-y-8 mx-auto">
-        <AuthHeader authMode={authMode}/>
-        <div></div>
-        <div className="bg-white">
-          <AuthMainButton authMode={authMode}/>
+        <AuthHeader authMode={authMode} />
+        <div className="bg-white w-full h-auto p-15 rounded-2xl space-y-4 shadow-2xl">
+          {inputForm[authMode]}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AuthPage
+export default AuthPage;
