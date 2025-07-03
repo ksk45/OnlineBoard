bc8389cd43d866ef3cf1dad86c8b1def9dfb4b3c
ksk45 <oooouu1k@gmail.com>
2025-07-03 23:18:43 +0900

    ログイン画面　バリデーション設定

--- frontend/src/components/Auth/Form/SignInForm.tsx
+++ frontend/src/components/Auth/Form/SignInForm.tsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
 import type { AuthMode } from "../../../types/auth";
+import {
+  validateEmailFormat,
+  validateMaxLength,
+  validateMinLength,
+  validatePasswordComplexity,
+  validateRequired,
+} from "../../../utils/validation";
 import AuthInput from "../AuthInput";
 import AuthMainButton from "../AuthMainButton";
 
@@ -13,8 +20,40 @@ const SignInForm = (props: SignInFormProps) => {
   // useState定義
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+
+  // バリデーション関数
+  const isValid = () => {
+    // emailバリデーション
+    const emailErr =
+      [
+        validateRequired(email, "メールアドレスは必須です"),
+        validateMaxLength(email, 100),
+        validateEmailFormat(email),
+      ].find(Boolean) || "";
+    setEmailError(emailErr);
+
+    // パスワードバリデーション
+    const passwordErr =
+      [
+        validateRequired(password, "パスワードは必須です"),
+        validateMinLength(password, 8),
+        validateMaxLength(password, 64),
+        validatePasswordComplexity(password),
+      ].find(Boolean) || "";
+    setPasswordError(passwordErr);
+
+    // バリデーションエラーがあればfalseを返す
+    return !emailErr && !passwordErr;
+  };
+
   // POSTapi呼び出し
   const handleSubmit = (e: React.FormEvent) => {
+    // submitのデフォルト挙動（ページ遷移）をキャンセル
+    e.preventDefault();
+    // バリデーションチェック
+    if (!isValid()) return;
   // const handleSubmit = async () => {
     // const res = await fetch("/api/login", {
     //   method: "POST",
@@ -22,7 +61,6 @@ const SignInForm = (props: SignInFormProps) => {
     //   body: JSON.stringify({ email, password }),
     // });
     /** バックエンド実装までのダミー st */
-    e.preventDefault();
     alert(`email: ${email} \npassword: ${password} \nログイン処理成功（ダミー）`);
     /** バックエンド実装までのダミー ed */
   };
@@ -31,13 +69,13 @@ const SignInForm = (props: SignInFormProps) => {
     <form onSubmit={handleSubmit} className="space-y-4">
       <AuthInput
         type="email"
-        errorMessage=""
+        errorMessage={emailError}
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <AuthInput
         type="pass"
-        errorMessage=""
+        errorMessage={passwordError}
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
