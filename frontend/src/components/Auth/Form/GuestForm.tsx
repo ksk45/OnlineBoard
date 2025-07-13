ca9565749899d36b3bfd599e9519daaf1f7e4f7c
ksk45 <oooouu1k@gmail.com>
2025-07-13 22:12:18 +0900

    ゲストログイン画面　フォーム作成

--- frontend/src/components/Auth/Form/GuestForm.tsx
+++ frontend/src/components/Auth/Form/GuestForm.tsx
@@ -1,8 +1,38 @@
+import React, { useState } from "react";
+import AuthInput from "../AuthInput";
+import { validateMaxLength, validateRequired } from "../../../utils/validation";
+import AuthMainButton from "../AuthMainButton";
 
 // ゲストログイン
-const GuestForm = () => (
-  //TODO: ゲストログイン画面作成時に定義
-  <></>
-);
+const GuestForm = () => {
+  // useState定義
+  const [name, setName] = useState("");
+  const [nameErr, setNameErr] = useState("");
 
-export default GuestForm
+  // エラー定義
+  const isValid = () => {
+    const nameErr =
+      [
+        validateRequired(name, "名前は必須です"),
+        validateMaxLength(name, 100),
+      ].find(Boolean) || "";
+    setNameErr(nameErr);
+
+    // バリデーションエラーがあればfalseを返す
+    return !nameErr;
+  };
+
+  return (
+    <form className="space-y-4">
+      <AuthInput
+        errorMessage={nameErr}
+        type="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <AuthMainButton authMode="guest" />
+    </form>
+  );
+};
+
+export default GuestForm;
