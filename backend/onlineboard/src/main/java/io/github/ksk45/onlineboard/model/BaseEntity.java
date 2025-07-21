8f25352674b0d363e359c82554440c40dc6233b4
ksk45 <oooouu1k@gmail.com>
2025-07-22 01:37:35 +0900

    バックエンド　監査カラム用Entityの作成

---
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/BaseEntity.java
@@ -0,0 +1,38 @@
+package io.github.ksk45.onlineboard.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+  @CreatedBy
+  @Column(name = "created_by", updatable = false)
+  private int createdBy;
+
+  @CreatedDate
+  @Column(name = "created_at", updatable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  private int updatedBy;
+
+  @LastModifiedDate
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+}
