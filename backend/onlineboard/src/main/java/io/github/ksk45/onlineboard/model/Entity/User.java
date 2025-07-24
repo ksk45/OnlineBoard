ba069137591dde637fbc019d9c73ec7b64b74f03
ksk45 <oooouu1k@gmail.com>
2025-07-24 21:32:19 +0900

    バックエンド　テーブル名変更

--- backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Entity/User.java
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Entity/User.java
@@ -6,15 +6,19 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Table(name = "users")
+@Table(name = "t_user")
 public class User extends AuditableEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
