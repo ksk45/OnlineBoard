c9bc0ca13f1b98993e2b54312d39eb13d016aaaa
ksk45 <oooouu1k@gmail.com>
2025-08-11 19:52:21 +0900

    メニュー画面　共通ヘッダーコンポーネント作成

---
+++ frontend/src/components/ui/UIHeader.tsx
@@ -0,0 +1,24 @@
+import type React from "react";
+
+// props定義
+type UIHeaderProps = {
+  leftContent?: React.ReactNode;
+  rightContent?: React.ReactNode;
+};
+
+const UIHeader = ( props: UIHeaderProps) => {
+  return (
+    <header className="bg-white shadow-md">
+      <div className="max-w-6xl px-4 py-3 flex justify-between items-center">
+        <div className="flex items-center">
+        {props.leftContent}
+        </div>
+        <div className="flex items-center">
+          {props.rightContent}
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default UIHeader;
