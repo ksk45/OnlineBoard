61bd8a91f16891134282077a87f85ce6a891249a
ksk45 <oooouu1k@gmail.com>
2025-08-18 02:54:28 +0900

    メニュー画面　新規作成ボタン作成

--- frontend/src/pages/MenuPage.tsx
+++ frontend/src/pages/MenuPage.tsx
@@ -8,6 +8,14 @@ const MenuPage = () => {
     <div>
       <MenuHeader />
       <h1>メニュー</h1>
+      <div className="flex items-center justify-between w-14/15">
+        <div>
+          {/* TODO:検索ボックス */}
+        </div>
+        <button className="h-9 px-4 bg-gradient-to-r from-blue-500 to-purple-600 rounded-md text-white">
+          + New WhiteBoard
+        </button>
+      </div>
       <p>ここにメニューの内容が表示されます。</p>
 	  <p>現在のユーザー</p>
     <p>ID: {userContext.user ? userContext.user.userId : "未ログイン"}</p>
