8d5592190252895e7f08ecaa9b268dad9ba88cce
ksk45 <oooouu1k@gmail.com>
2025-08-30 23:25:51 +0900

    新規作成モーダル　DB登録確認

--- backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/Controller/BoardController.java
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/Controller/BoardController.java
@@ -1,13 +1,16 @@
 package io.github.ksk45.onlineboard.Controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.ksk45.onlineboard.Model.Entity.Board;
 import io.github.ksk45.onlineboard.Model.Form.Board.BoardCreateForm;
+import io.github.ksk45.onlineboard.Model.Form.Common.UserContext;
+import io.github.ksk45.onlineboard.Service.Board.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
@@ -15,14 +18,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Slf4j
 @RequestMapping("board")
 public class BoardController {
+
+  private final BoardService boardService;
   
   @PostMapping("/create")
-  public String postMethodName(@RequestBody BoardCreateForm boardCreateForm) {
-    System.out.println("debug");
-      //TODO: process POST request
-      
-      // return entity;
-      return "return";
+  public String boardCreate(@RequestBody BoardCreateForm boardCreateForm) {
+
+    // テストコード
+    boardCreateForm.setUserContext(UserContext.builder().userId(0).build());
+    // テストコード
+
+    Board board = boardService.boardCreate(boardCreateForm);
+    
+    // return entity;
+    return "return";
   }
   
 }
--- backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Common/UserContext.java
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Common/UserContext.java
@@ -1,8 +1,10 @@
 package io.github.ksk45.onlineboard.Model.Form.Common;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserContext {
   
   private Integer userId;
