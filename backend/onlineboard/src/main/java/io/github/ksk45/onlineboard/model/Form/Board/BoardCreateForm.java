689fa59dbf2a13d2cd1caf343450c63dcc4499ca
ksk45 <oooouu1k@gmail.com>
2025-09-01 23:46:46 +0900

    新規作成モーダル　バリデーション実装

--- backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Board/BoardCreateForm.java
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Board/BoardCreateForm.java
@@ -1,13 +1,18 @@
 package io.github.ksk45.onlineboard.Model.Form.Board;
 
 import io.github.ksk45.onlineboard.Model.Form.Common.UserContext;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class BoardCreateForm {
   
+  @NotBlank(message = "{VALIDATION_REQUIRED}")
+  @Size(max = 50, message = "{VALIDATION_MAX_LENGTH}")
   private String wbName;
 
+  @Size(max = 200, message = "{VALIDATION_MAX_LENGTH}")
   private String wbDesc;
 
   private UserContext userContext;
