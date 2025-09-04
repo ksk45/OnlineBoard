b917e1837bcd3c4c023f9bb7f479f0acd5807c9c
ksk45 <oooouu1k@gmail.com>
2025-09-04 23:53:01 +0900

    バックエンド　新規バリデーション作成

--- backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Board/BoardCreateForm.java
+++ backend/onlineboard/src/main/java/io/github/ksk45/onlineboard/model/Form/Board/BoardCreateForm.java
@@ -1,6 +1,7 @@
 package io.github.ksk45.onlineboard.Model.Form.Board;
 
 import io.github.ksk45.onlineboard.Model.Form.Common.UserContext;
+import io.github.ksk45.onlineboard.Validation.ValidUserContext;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -15,5 +16,6 @@ public class BoardCreateForm {
   @Size(max = 200, message = "{VALIDATION_MAX_LENGTH}")
   private String wbDesc;
 
+  @ValidUserContext(message = "{VALIDATION_USER_CONTEXT_INVALID}") 
   private UserContext userContext;
 }
